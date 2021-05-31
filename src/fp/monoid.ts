644d4f73153d2844de336925c9b1445837f8338f
ssshuai1999 <ssshuai1999@gmail.com>
2021-05-31 16:00:29 +0800

    feat: fp -> monoid

--- src/fp/monoid.ts
+++ src/fp/monoid.ts
@@ -2,210 +2,204 @@
  * 幺半群
  */
 
-import { Semigroup } from 'fp-ts/lib/Semigroup'
-import { getStructMonoid, Monoid, fold } from 'fp-ts/lib/Monoid'
-import { getApplyMonoid, some, none, getFirstMonoid, Option, getLastMonoid } from 'fp-ts/lib/Option'
+import { Semigroup } from "fp-ts/lib/Semigroup";
+import { getStructMonoid, Monoid, fold } from "fp-ts/lib/Monoid";
+import {
+  getApplyMonoid,
+  some,
+  none,
+  getFirstMonoid,
+  Option,
+  getLastMonoid,
+} from "fp-ts/lib/Option";
 
 export default (() => {
-    /** 
-     * 半群补货了 "合并" 值的概念（通过 concat），半群是任何碰巧有一个特殊值的半群，这个特殊值相对于凹而言是 "中性" 的。
-    */
+  /**
+   * 半群补货了 "合并" 值的概念（通过 concat），半群是任何碰巧有一个特殊值的半群，这个特殊值相对于凹而言是 "中性" 的。
+   */
 
-    /** 
-     * Type class definition 类型定义
-     * 通常在 fp-ts 中， fp-ts/lib/Monoid 模块中包含的类型类 Monoid 被实现为一个 Typescript 接口，其中的 nentral 值为空
-    */
-   
-    interface Monoid<A> extends Semigroup<A> {
-        readonly empty: A
-    }
-    
-    /**
-     * 以下条件必须成立
-     * right identity 右身份 concat(x, empty) = x 
-     * left identity 左身份 concat(empty, x) = x
-     * 无论我们将值放在 concat 的哪一边，它都必须与值没有任何区别。
-     * 注意：如果一个空值存在，那么它是唯一的。
-     */
+  /**
+   * Type class definition 类型定义
+   * 通常在 fp-ts 中， fp-ts/lib/Monoid 模块中包含的类型类 Monoid 被实现为一个 Typescript 接口，其中的 nentral 值为空
+   */
 
-    /**
-     * Instance 实例
-     * 我们之前使用的大多半群实际上是幺半群
-     */
+  interface Monoid<A> extends Semigroup<A> {
+    readonly empty: A;
+  }
 
-    /** number `Monoid` under addition */
-    const monoidSum: Monoid<number> = {
-        concat: (x, y) => x + y,
-        empty: 0
-    }
+  /**
+   * 以下条件必须成立
+   * right identity 右身份 concat(x, empty) = x
+   * left identity 左身份 concat(empty, x) = x
+   * 无论我们将值放在 concat 的哪一边，它都必须与值没有任何区别。
+   * 注意：如果一个空值存在，那么它是唯一的。
+   */
 
-    /** number `Monoid` under multiplication */
-    const monoidProduct: Monoid<number> = {
-        concat: (x, y) => x * y,
-        empty: 1
-    }
+  /**
+   * Instance 实例
+   * 我们之前使用的大多半群实际上是幺半群
+   */
 
-    const monoidString: Monoid<string> = {
-        concat: (x, y) => x + y,
-        empty: ''
-    }
-    
-    const monoidAll: Monoid<boolean> = {
-        concat: (x, y) => x && y,
-        empty: true
-    }
+  /** number `Monoid` under addition */
+  const monoidSum: Monoid<number> = {
+    concat: (x, y) => x + y,
+    empty: 0,
+  };
 
-    const monoidAny: Monoid<boolean> = {
-        concat: (x, y) => x || y,
-        empty: false
-    }
+  /** number `Monoid` under multiplication */
+  const monoidProduct: Monoid<number> = {
+    concat: (x, y) => x * y,
+    empty: 1,
+  };
 
-    /**
-     * 你可能想知道是否所有的半群都是幺半群，但事实并非如此，作为一个反例，考虑下面的半群。
-     */
-    const semigroupSpace: Semigroup<string> = {
-        concat: (x,y) => x + ' ' + y
-    }
+  const monoidString: Monoid<string> = {
+    concat: (x, y) => x + y,
+    empty: "",
+  };
 
+  const monoidAll: Monoid<boolean> = {
+    concat: (x, y) => x && y,
+    empty: true,
+  };
 
-    /**
-     * 我们不能找到一个空值，比如 concat(x, empty) = x
-     * 让我们为更复杂的类型编写一些 Monoid 实例
-     */
-    type Point = {
-        x: number, 
-        y: number
-    }
+  const monoidAny: Monoid<boolean> = {
+    concat: (x, y) => x || y,
+    empty: false,
+  };
 
-    /**
-     * 如果我们可以为每个字段提供一个 Monoid 实例
-     */
-    
-    const monoidPoint: Monoid<Point> = getStructMonoid({
-        x: monoidSum,
-        y: monoidSum,
-    })
+  /**
+   * 你可能想知道是否所有的半群都是幺半群，但事实并非如此，作为一个反例，考虑下面的半群。
+   */
+  const semigroupSpace: Semigroup<string> = {
+    concat: (x, y) => x + " " + y,
+  };
 
-    /**
-     * 我们可以继续使用刚刚定义的实例提供 getStructMonoid
-     */
+  /**
+   * 我们不能找到一个空值，比如 concat(x, empty) = x
+   * 让我们为更复杂的类型编写一些 Monoid 实例
+   */
+  type Point = {
+    x: number;
+    y: number;
+  };
 
-    type Vector = {
-        from: Point,
-        to: Point
-    }
+  /**
+   * 如果我们可以为每个字段提供一个 Monoid 实例
+   */
 
-    const monoidVector: Monoid<Vector> = getStructMonoid({
-        from: monoidPoint,
-        to: monoidPoint,
-    })
+  const monoidPoint: Monoid<Point> = getStructMonoid({
+    x: monoidSum,
+    y: monoidSum,
+  });
 
-    /**
-     * Folding 
-     * 当使用 monoid 而不是半群时， folding 就更简单了，我们不需要显示地提供初始值（实现可以使用 monoid 的空值）
-     */
+  /**
+   * 我们可以继续使用刚刚定义的实例提供 getStructMonoid
+   */
 
-    fold(monoidSum)([1,2,3,4])
-    fold(monoidProduct)([1,2,3,4])
-    fold(monoidString)(['a', 'b', 'c'])
-    fold(monoidAll)([true, true, true])
-    fold(monoidAny)([false, false, false])
+  type Vector = {
+    from: Point;
+    to: Point;
+  };
 
-    /**
-     * 我们已经知道，给定 A 的版区你实力，我们可以导出选项 <A> 的半群实例
-     * 如果我们能为 A 找到一个 monoid 实例，那么我们就能为 Option<A> 派生一个 monoid 实例（通过 getApplyMonoid），它的工作原理如下
-     * 
-     * x                y               concat(x, y)
-     * -----------------------------------------------------
-     * none             none            none
-     * -----------------------------------------------------
-     * some(a)          none            none
-     * -----------------------------------------------------
-     * none             some(a)         none
-     * -----------------------------------------------------
-     * some(a)          some(b)         some(concat(a, b))
-     * -----------------------------------------------------
-     */
+  const monoidVector: Monoid<Vector> = getStructMonoid({
+    from: monoidPoint,
+    to: monoidPoint,
+  });
 
-    const M = getApplyMonoid(monoidSum)
+  /**
+   * Folding
+   * 当使用 monoid 而不是半群时， folding 就更简单了，我们不需要显示地提供初始值（实现可以使用 monoid 的空值）
+   */
 
-    M.concat(some(1), none) // none
-    M.concat(some(1), some(2)) // some(3)
-    M.concat(some(1), M.empty) // some(1)
+  fold(monoidSum)([1, 2, 3, 4]);
+  fold(monoidProduct)([1, 2, 3, 4]);
+  fold(monoidString)(["a", "b", "c"]);
+  fold(monoidAll)([true, true, true]);
+  fold(monoidAny)([false, false, false]);
 
-    /**
-     * 对于 Option<A>，我们可以到处另外两个幺半群
-     * getFirstMonoid ...
-     * 返回最左的非 none 值
-     * 
-     * x                y               concat(x, y)
-     * -----------------------------------------------------
-     * none             none            none
-     * -----------------------------------------------------
-     * some(a)          none            some(a)
-     * -----------------------------------------------------
-     * none             some(a)         some(a)
-     * -----------------------------------------------------
-     * some(a)          some(b)         some(b)
-     * -----------------------------------------------------
-     */
+  /**
+   * 我们已经知道，给定 A 的版区你实力，我们可以导出选项 <A> 的半群实例
+   * 如果我们能为 A 找到一个 monoid 实例，那么我们就能为 Option<A> 派生一个 monoid 实例（通过 getApplyMonoid），它的工作原理如下
+   *
+   * x                y               concat(x, y)
+   * -----------------------------------------------------
+   * none             none            none
+   * -----------------------------------------------------
+   * some(a)          none            none
+   * -----------------------------------------------------
+   * none             some(a)         none
+   * -----------------------------------------------------
+   * some(a)          some(b)         some(concat(a, b))
+   * -----------------------------------------------------
+   */
 
-    const M2 = getFirstMonoid<number>()
-    M2.concat(some(1), none)     // some(1)
-    M2.concat(some(1), some(2))  // some(2)
+  const M = getApplyMonoid(monoidSum);
 
-    /**
-     * 例如，getLastMonoid 可用于管理可选值
-     */
+  M.concat(some(1), none); // none
+  M.concat(some(1), some(2)); // some(3)
+  M.concat(some(1), M.empty); // some(1)
 
-    interface Settings {
-        /** Controls the font family */
-        fontFamily: Option<string>
+  /**
+   * 对于 Option<A>，我们可以到处另外两个幺半群
+   * getFirstMonoid ...
+   * 返回最左的非 none 值
+   *
+   * x                y               concat(x, y)
+   * -----------------------------------------------------
+   * none             none            none
+   * -----------------------------------------------------
+   * some(a)          none            some(a)
+   * -----------------------------------------------------
+   * none             some(a)         some(a)
+   * -----------------------------------------------------
+   * some(a)          some(b)         some(b)
+   * -----------------------------------------------------
+   */
 
-        /** Controls the font size in pixels */
-        fontSize: Option<number>
+  const M2 = getFirstMonoid<number>();
+  M2.concat(some(1), none); // some(1)
+  M2.concat(some(1), some(2)); // some(2)
 
-        /** Limit the width the minimap to render at most a certain number of columns. */
-        maxColumn: Option<number>
-    }
+  /**
+   * 例如，getLastMonoid 可用于管理可选值
+   */
 
-    const monoidSettings: Monoid<Settings> = getStructMonoid({
-        fontFamily: getLastMonoid<string>(),
-        fontSize: getLastMonoid<number>(),
-        maxColumn: getLastMonoid<number>(),
-    })
+  interface Settings {
+    /** Controls the font family */
+    fontFamily: Option<string>;
 
+    /** Controls the font size in pixels */
+    fontSize: Option<number>;
 
-    const workspaceSettings: Settings = {
-        fontFamily: some('Courier'),
-        fontSize: none,
-        maxColumn: some(80)
-    }
+    /** Limit the width the minimap to render at most a certain number of columns. */
+    maxColumn: Option<number>;
+  }
 
-    const userSettings: Settings = {
-        fontFamily: some('Fira Code'),
-        fontSize: some(12),
-        maxColumn: none
-    }
+  const monoidSettings: Monoid<Settings> = getStructMonoid({
+    fontFamily: getLastMonoid<string>(),
+    fontSize: getLastMonoid<number>(),
+    maxColumn: getLastMonoid<number>(),
+  });
 
-    /** userSettings overrides workspaceSettings */
-    monoidSettings.concat(workspaceSettings, userSettings)
-    /**
-     * {
-     *   fontFmaily: some("Fira Code"),
-     *   fontSize: some(12),
-     *   maxColumn: some(80)
-     * }
-     */
+  const workspaceSettings: Settings = {
+    fontFamily: some("Courier"),
+    fontSize: none,
+    maxColumn: some(80),
+  };
 
+  const userSettings: Settings = {
+    fontFamily: some("Fira Code"),
+    fontSize: some(12),
+    maxColumn: none,
+  };
 
-
-
-
-    
-
-
-
-
-
-})()
+  /** userSettings overrides workspaceSettings */
+  monoidSettings.concat(workspaceSettings, userSettings);
+  /**
+   * {
+   *   fontFmaily: some("Fira Code"),
+   *   fontSize: some(12),
+   *   maxColumn: some(80)
+   * }
+   */
+})();
--- src/fp/semigroup.ts
+++ src/fp/semigroup.ts
@@ -12,16 +12,12 @@ import {
   fold,
   semigroupSum,
   semigroupProduct,
-  semigroupAny
-} from 'fp-ts/lib/Semigroup'
-import {
-  getApplySemigroup,
-  some,
-  none
-} from 'fp-ts/lib/Option'
-import { getMonoid } from 'fp-ts/lib/Array'
-import { ordNumber, contramap } from 'fp-ts/lib/Ord'
-import { SemigroupAny } from 'fp-ts/lib/boolean'
+  semigroupAny,
+} from "fp-ts/lib/Semigroup";
+import { getApplySemigroup, some, none } from "fp-ts/lib/Option";
+import { getMonoid } from "fp-ts/lib/Array";
+import { ordNumber, contramap } from "fp-ts/lib/Ord";
+import { SemigroupAny } from "fp-ts/lib/boolean";
 
 export default (() => {
   /**
@@ -43,9 +39,9 @@ export default (() => {
    * Type class definition 类型定义
    * 通常在 fp-ts 中，fp-ts/lib/semigroup 模块中包含的类型类半群被实现为一个 Typescript 接口，其中操作 * 被命名为 concat
    */
-  
+
   interface Semigroup<A> {
-    concat: (x: A, y: A) => A
+    concat: (x: A, y: A) => A;
   }
 
   /**
@@ -70,42 +66,42 @@ export default (() => {
    * 半群 (number, *)
    */
   const semigroupProduct: Semigroup<number> = {
-    concat: (x, y) => x * y
-  }
+    concat: (x, y) => x * y,
+  };
 
   /** 注意，可以为同一类型定义不用的半群实例，这是半群 (number, +) 的实现，其中 + 是通常的数的加法 */
   const semigroupSum: Semigroup<number> = {
-    concat: (x, y) => x + y
-  }
+    concat: (x, y) => x + y,
+  };
 
   /** 另一个例子，这次是带字符串的 */
   const semigroupString: Semigroup<string> = {
-    concat: (x, y) => x + y
-  }
+    concat: (x, y) => x + y,
+  };
 
   /** 如果给定一个类型 A, 你不能在 A 上找到一个关联运算，那该怎么办？ 可以使用以下结构为每个类型创建（平凡的）半群实例 */
 
   /** 永远返回第一个参数 */
   function getFirstSemigroup<A = never>(): Semigroup<A> {
-    return { concat: (x, y) => x }
+    return { concat: (x, y) => x };
   }
 
   /** 永远返回第二个参数 */
   function getLastSemigroup<A = never>(): Semigroup<A> {
-    return { concat: (x, y) => y }
+    return { concat: (x, y) => y };
   }
 
   /** 另一种技巧是定义阵列 <A>(*)的半群实例，称为 A 的自由半群 */
   function getArraySemigroup<A = never>(): Semigroup<Array<A>> {
-    return { concat: (x, y) => x.concat(y) }
+    return { concat: (x, y) => x.concat(y) };
   }
 
   /** 并将 A 的元素映射到 Array<a> 的单个元素上 */
   function of<A>(a: A): Array<A> {
-    return [a]
+    return [a];
   }
 
-  /** 
+  /**
    * (*) 严格地说是 A 的非空数组的半群实例
    * 注意，这里 concat 是本机数组方法，它在一定程度上解释了半群运算名称的初始选择
    * A 的自由半群是元素都可能是 a 的元素的非空有限序列的半群
@@ -118,22 +114,22 @@ export default (() => {
    */
 
   /** 在两个数中拿走最小数 */
-  const semigroupMin: Semigroup<number> = getMeetSemigroup(ordNumber)
+  const semigroupMin: Semigroup<number> = getMeetSemigroup(ordNumber);
 
   /** 在两个数中拿走最大数 */
-  const semigroupMax: Semigroup<number> = getJoinSemigroup(ordNumber)
+  const semigroupMax: Semigroup<number> = getJoinSemigroup(ordNumber);
 
-  semigroupMin.concat(2, 1) // logs => `1`
-  semigroupMax.concat(2, 1) // logs => `2`
+  semigroupMin.concat(2, 1); // logs => `1`
+  semigroupMax.concat(2, 1); // logs => `2`
 
   /**
    * 让我们为更复杂的类型编写一些半群实例
    */
 
   type Point = {
-    x: number,
-    y: number
-  }
+    x: number;
+    y: number;
+  };
 
   /** （手写） fp-ts 提供了对应的模块 */
   // const semigroupPoint: Semigroup<Point> = {
@@ -148,43 +144,39 @@ export default (() => {
    */
   const semigroupPoint: Semigroup<Point> = getStructSemigroup({
     x: semigroupSum,
-    y: semigroupSum
-  })
+    y: semigroupSum,
+  });
 
   /** 我们可以继续使用刚刚定义的实例来填充 getstruct 半群 */
 
   type Vector = {
-    from: Point,
-    to: Point
-  }
+    from: Point;
+    to: Point;
+  };
 
   const semigroupVector: Semigroup<Vector> = getStructSemigroup({
     from: semigroupPoint,
-    to: semigroupPoint
-  })
+    to: semigroupPoint,
+  });
 
   /**
    * Getstruct 半群不是 fp-ts 提供的唯一组合子，这里有一个组合子，它允许推到函数的半群实例：给定 s 的半群实例，我们可以推到出具有签名（a: A）=> s 的函数的半群实例，对于所有 A
    */
 
   /** `semigroupAll` is the boolean semigroup under conjunction */
-  const semigroupPredicate: Semigroup<(p: Point) => boolean> = getFunctionSemigroup(
-    semigroupAll
-  )<Point>()
-
+  const semigroupPredicate: Semigroup<(p: Point) => boolean> =
+    getFunctionSemigroup(semigroupAll)<Point>();
 
   /** 现在我们可以 "合并" point 上的两个 predicates */
-  const isPositiveX = (p: Point): boolean => p.x >= 0
-  const isPositiveY = (p: Point): boolean => p.y >= 0
+  const isPositiveX = (p: Point): boolean => p.x >= 0;
+  const isPositiveY = (p: Point): boolean => p.y >= 0;
 
-  const isPositiveXY = semigroupPredicate.concat(isPositiveX, isPositiveY)
+  const isPositiveXY = semigroupPredicate.concat(isPositiveX, isPositiveY);
 
-  isPositiveXY({ x: 1, y: 1 })    // logs => `true`
-  isPositiveXY({ x: 1, y: -1 })   // logs => `false`
-  isPositiveXY({ x: -1, y: 1 })   // logs => `false`
-  isPositiveXY({ x: -1, y: -1 })  // logs => `false`
-  
-
+  isPositiveXY({ x: 1, y: 1 }); // logs => `true`
+  isPositiveXY({ x: 1, y: -1 }); // logs => `false`
+  isPositiveXY({ x: -1, y: 1 }); // logs => `false`
+  isPositiveXY({ x: -1, y: -1 }); // logs => `false`
 
   /**
    * Folding
@@ -192,18 +184,18 @@ export default (() => {
    * fold 函数采用一个半群实例，一个初始值，一个元素数组
    */
 
-  const sum = fold(semigroupSum)
+  const sum = fold(semigroupSum);
 
-  sum(0, [1,2,3,4]) // 10
-  
-  const product = fold(semigroupProduct)
+  sum(0, [1, 2, 3, 4]); // 10
 
-  product(1, [1,2,3,4]) // 24
+  const product = fold(semigroupProduct);
+
+  product(1, [1, 2, 3, 4]); // 24
 
   /**
    * Semigroups for type constructors 类型构造函数的半群
    * 如果我们想要合并两个 Option<A>? 有四种情况:
-   * 
+   *
    * x            y            concat(x, y)
    * -----------------------------------------
    * none         none          none
@@ -216,15 +208,14 @@ export default (() => {
    * -----------------------------------------
    */
 
-  /** 
+  /**
    * 最后一个有个问题，我们需要一些东西来 `合并` 两个 A
    * 这就是半群所做的！我们可以要求 A 的半群实例，然后推到出 Option<A> 的半群实例。这就是 getApplySemigroup 的工作原理
    */
-  const S = getApplySemigroup(semigroupSum)
-  S.concat(some(1), none)     // none
-  S.concat(some(1), some(2))  // some(3)
+  const S = getApplySemigroup(semigroupSum);
+  S.concat(some(1), none); // none
+  S.concat(some(1), some(2)); // some(3)
 
-  
   /**
    * 我们已经看到，当我们想要 `concat`，`merge` or `combine` (无论什么词给你最好的直觉) 几个数据集合成一个时，半群帮助我们。
    * 我们用最后一个例子来总结以下
@@ -234,11 +225,11 @@ export default (() => {
    * 让我们假设您正在构建一个系统，其中存储的客户记录如下所示：
    */
   interface Customer {
-    name: string,
-    favouriteThings: Array<string>
-    registeredAt: number
-    lastUpdatedAt: number
-    hasMadePurchase: boolean
+    name: string;
+    favouriteThings: Array<string>;
+    registeredAt: number;
+    lastUpdatedAt: number;
+    hasMadePurchase: boolean;
   }
 
   /**
@@ -255,25 +246,25 @@ export default (() => {
     // keep the most recent data
     lastUpdatedAt: getJoinSemigroup(ordNumber),
     // Boolean semigroup under disjunction
-    hasMadePurchase: SemigroupAny
-  })
+    hasMadePurchase: SemigroupAny,
+  });
 
   semigroupCustomer.concat(
     {
-      name: 'Giulio',
-      favouriteThings: ['math', 'climbing'],
+      name: "Giulio",
+      favouriteThings: ["math", "climbing"],
       registeredAt: new Date(2018, 1, 20).getTime(),
       lastUpdatedAt: new Date(2018, 2, 18).getTime(),
-      hasMadePurchase: false
+      hasMadePurchase: false,
     },
     {
-      name: 'Giulio Canti',
-      favouriteThings: ['functional programming'],
+      name: "Giulio Canti",
+      favouriteThings: ["functional programming"],
       registeredAt: new Date(2018, 1, 22).getTime(),
       lastUpdatedAt: new Date(2018, 2, 9).getTime(),
-      hasMadePurchase: true
+      hasMadePurchase: true,
     }
-  )
+  );
 
   /*
     { name: 'Giulio Canti',
@@ -282,9 +273,8 @@ export default (() => {
     lastUpdatedAt: 1521327600000, // new Date(2018, 2, 18).getTime()
     hasMadePurchase: true }
   */
-  
+
   /**
    * 函数 getMonoid 返回数组 < 字符串 > 的半群。实际上它返回的不仅仅是一个半群: monoid。
    */
-
-})()
+})();
